185862738b7b490a4bdd56fa0c31f1725d15dad1
Zhaopeng Zhang <zhangzhaopeng@mail.nankai.edu.cn>
2016-12-02 11:27:06 +0800

    split fasta into single sequence fasta to sourcedir.

---
+++ splitfasta.py
@@ -0,0 +1,41 @@
+#coding:utf-8
+"""
+split fastafile into single sequence fastafile.
+length>30 and no X in seq.
+"""
+
+import os
+import log
+import sys
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+
+def splitfasta(fastafile, sourcedir):
+    mylog = log.Terminal_log()
+    if not os.path.exists(sourcedir):
+        os.makedirs(sourcedir)
+        mylog.info("make sourcedir.")
+    if not os.path.exists(fastafile):
+        mylog.fatal("fastafile not existed.")
+        sys.exit(1)
+    for seq_record in SeqIO.parse(fastafile, 'fasta'):
+        seq = seq_record.seq
+        idd = seq_record.id.split("|")[1]
+        des = seq_record.description
+        # filter
+        if len(seq) >= 30 and 'X' not in seq:
+            rec = SeqRecord(seq,
+            id=seq_record.id,
+            description=des)
+            target = os.path.join(sourcedir, "%s.fasta" %idd)
+            if os.path.exists(target):
+                mylog.error("%s existed." % target)
+            SeqIO.write([rec], target, 'fasta')
+            mylog.debug("%s splited." % idd)
+    mylog.done("done.")
+
+if __name__ == '__main__':
+    fastafile = "/home/zzp/DATABASE/Human/UP000005640_9606.fasta"
+    sourcedir = "/home/zzp/DATABASE/Human/sequences/"
+    splitfasta(fastafile, sourcedir)
