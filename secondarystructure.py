2c62865a608071d24d6d7f136a8f195c61169f4b
Zhaopeng Zhang <zhangzhaopeng@mail.nankai.edu.cn>
2016-12-03 00:30:10 +0800

    multiprocessing psipred.

--- secondarystructure.py
+++ secondarystructure.py
@@ -3,37 +3,35 @@
 threadpool psipred cal ss.
 """
 import os
-import threadpool
+import multiprocessing
 from glob import glob
 import log
 
 mylog = log.Terminal_log()
-
+PSIPRED = '/home/zzp/program/release/PSIPRED/4.01/runpsipred'
 def psipred(fastafile):
     idd = os.path.basename(fastafile).split('.')[0]
-    order = " ".join(('psipred', fastafile))
+    order = " ".join((PSIPRED, fastafile))
+    mylog.debug("order: %s" % order)
     os.system(order)
     return idd
 
-def callback(requests, result):
-    if result == True:
-        mylog.done("%s done." % result)
-    else:
-        mylog.error("error.")
-
 def cal(seqdir):
-    filenames = []
-    for filename in glob("*.fasta"):
-        f1 = os.path.splitext(filename)+".ss2"
-        f2 = os.path.splitext(filename)+".horiz"
+    pool = multiprocessing.Pool(processes=4)
+    results = []
+    for filename in glob(os.path.join(seqdir, "*.fasta")):
+        f1 = os.path.splitext(filename)[0] + ".ss2"
+        f2 = os.path.splitext(filename)[0] + ".horiz"
+        mylog.debug("f1: %s, f2: %s" % (f1, f2))
         if not os.path.exists(f1) or not os.path.exists(f2):
-            filenames.append(filename)
+            results.append(pool.apply_async(psipred, (filename,)))
+    for r in results:
+        mylog.info("%s done." % r.get())
+    pool.close()
+    pool.join()
+    mylog.done("done.")
 
-    pool = threadpool.ThreadPool(8)
-    requests = threadpool.makeRequests(psipred, filenames, callback)
-    for req in requests: pool.putRequest(req)
-    pool.wait()
-
+    
 if __name__ == "__main__":
-    seqdir = "/home/zzp/DATABASE/Human/sequences/"
+    seqdir = "/home/zzp/DATABASE/Human/sequences/test/"
     cal(seqdir)
