bcdd73e09bb0056b22300c849c8d30d987150377
biolab <biolab@mathbio.nankai.edu.cn>
2016-12-03 14:01:20 +0800

    put into biolab.

---
+++ log.py
@@ -0,0 +1,224 @@
+# coding:utf-8
+'''
+Author       :  ZZP
+Mail         :  zhangzhaopeng@mail.nankai.edu.cn
+Created Time :  2016-11-16 19:12:09
+
+Description  :  log sth with tags and colors using ASSIC.
+
+'''
+
+
+class Terminal_log:
+    """
+    log into screen with tags and colors.
+
+    Members
+    -------
+    debug(self, msg)
+        debug message.
+    info(self, msg)
+        info message.
+    warn(self, msg)
+        warn message.
+    error(self, msg)
+        error message.
+    fatal(self, msg)
+        fatal error message.
+    done(self, msg)
+        done message.
+    """
+
+    def __init__(self, logfilename=None, tag=True, color=True, brief=False):
+        """
+        constructor of Terminal_log.
+
+        Parameters
+        ----------
+        logfilename: str
+            default: None
+            log into file at the same time of log into screen if it's true.
+        tag: bool
+            default: True
+            log without tags if it's false.
+        color: bool
+            default: True
+            log without colors if it's false.
+        brief: bool
+            default: False
+            don't print debug log if it's true.
+        """
+        if color:
+            self.__ENDC__ = "\033[0m"
+            self.__INFO__ = "\033[32m"
+            self.__WARN__ = "\033[33m"
+            self.__ERROR__ = "\033[35m"
+            self.__FATAL__ = "\033[31m"
+            self.__DONE__ = "\033[36m"
+        else:
+            self.__ENDC__ = ""
+            self.__INFO__ = ""
+            self.__WARN__ = ""
+            self.__ERROR__ = ""
+            self.__FATAL__ = ""
+            self.__DONE__ = ""
+        self._logfilename = logfilename
+        self._tag = tag
+        self._brief = brief
+
+    def __logfw__(self, msg):
+        """
+        write to self._logfilename
+
+        Parameters
+        ----------
+        msg: str
+            message that would be written to self._logfilename
+        """
+        fw = open(self._logfilename, 'a')
+        fw.write(msg + "\n")
+        fw.close()
+
+    def debug(self, msg):
+        """
+        log debug message.
+
+        Parameters
+        ----------
+        msg: str
+            print debug message.
+            if self._brief == True, don't print.
+
+        Examples
+        --------
+        >>> mylog = Terminal_log()
+        >>> mylog.debug("this is debug")
+        [d] this is debug
+        """
+        if not self._brief:
+            msg = "[d] " + msg
+            print msg
+            if self._logfilename:
+                self.__logfw__(msg)
+
+    def info(self, msg):
+        """
+        log info message.
+
+        Parameters
+        ----------
+        msg: str
+            print info message.
+
+        Examples
+        --------
+        >>> mylog = Terminal_log()
+        >>> mylog.info("this is info")
+        \033[32m[+] this is info\033[0m
+        """
+        if self._tag:
+            msg = "[+] " + msg
+        result = self.__INFO__ + msg + self.__ENDC__
+        print(result)
+        if self._logfilename:
+            self.__logfw__(msg)
+
+    def warn(self, msg):
+        """
+        log warn message.
+
+        Parameters
+        ----------
+        msg: str
+            print warn message.
+
+        Examples
+        --------
+        >>> mylog = Terminal_log()
+        >>> mylog.warn("this is warn")
+        \033[33m[!] this is warn\033[0m
+        """
+        if self._tag:
+            msg = "[!] " + msg
+        result = self.__WARN__ + msg + self.__ENDC__
+        print(result)
+        if self._logfilename:
+            self.__logfw__(msg)
+
+    def error(self, msg):
+        """
+        log error message.
+
+        Parameters
+        ----------
+        msg: str
+            print error message.
+
+        Examples
+        --------
+        >>> mylog = Terminal_log()
+        >>> mylog.error("this is error")
+        \033[31m[-] this is error\033[0m
+        """
+        if self._tag:
+            msg = "[-] " + msg
+        result = self.__ERROR__ + msg + self.__ENDC__
+        print(result)
+        if self._logfilename:
+            self.__logfw__(msg)
+
+    def fatal(self, msg):
+        """
+        log fatal error message.
+
+        Parameters
+        ----------
+        msg: str
+            print fatal error message.
+
+        Examples
+        --------
+        >>> mylog = Terminal_log()
+        >>> mylog.fatal("this is fatal error")
+        \033[35m[×] this is fatal error\033[0m
+        """
+        if self._tag:
+            msg = "[×] " + msg
+        result = self.__FATAL__ + msg + self.__ENDC__
+        print(result)
+        if self._logfilename:
+            self.__logfw__(msg)
+
+    def done(self, msg):
+        """
+        log done message.
+
+        Parameters
+        ----------
+        msg: str
+            print done message.
+
+        Examples
+        --------
+        >>> mylog = Terminal_log()
+        >>> mylog.done("done.")
+        \033[35m[✓] done.\033[0m
+        """
+        if self._tag:
+            msg = "[✓] " + msg
+        result = self.__DONE__ + msg + self.__ENDC__
+        print(result)
+        if self._logfilename:
+            self.__logfw__(msg)
+
+if __name__ == '__main__':
+    """
+    test Terminal_log
+    """
+    m = Terminal_log(color=True)
+    m.debug("this is debug")
+    m.info("this is info")
+    m.warn("this is warn")
+    m.error("this is error")
+    m.fatal("this is fatal")
+    m.done("sth is done.")
--- secondarystructure.py
+++ secondarystructure.py
@@ -4,11 +4,13 @@ threadpool psipred cal ss.
 """
 import os
 import multiprocessing
+import time
 from glob import glob
 import log
 
 mylog = log.Terminal_log()
 PSIPRED = '/home/zzp/program/release/PSIPRED/4.01/runpsipred'
+
 def psipred(fastafile):
     idd = os.path.basename(fastafile).split('.')[0]
     order = " ".join((PSIPRED, fastafile))
@@ -17,21 +19,34 @@ def psipred(fastafile):
     return idd
 
 def cal(seqdir):
-    pool = multiprocessing.Pool(processes=4)
+
+    start = time.time()
+    pool = multiprocessing.Pool(processes=2)
     results = []
+    '''
     for filename in glob(os.path.join(seqdir, "*.fasta")):
         f1 = os.path.splitext(filename)[0] + ".ss2"
         f2 = os.path.splitext(filename)[0] + ".horiz"
-        mylog.debug("f1: %s, f2: %s" % (f1, f2))
+        # mylog.debug("f1: %s, f2: %s" % (f1, f2))
         if not os.path.exists(f1) or not os.path.exists(f2):
             results.append(pool.apply_async(psipred, (filename,)))
+    '''
+    filename = os.path.join(seqdir, "W5XKT8.fasta")
+    results.append(pool.apply_async(psipred,(filename,)))
+    filename = os.path.join(seqdir, "W5XKT8.fasta")
+    results.append(pool.apply_async(psipred,(filename,)))
+    num = 0
     for r in results:
-        mylog.info("%s done." % r.get())
+        num += 1
+        t = time.time()-start
+        mylog.info("num: %d, %s done, already %.2f min, ave time: %.2f min." % (num, r.get(), t/60., t*1.0/num/60.))
+
     pool.close()
     pool.join()
     mylog.done("done.")
+    # 注意psipred结果位置.
 
     
 if __name__ == "__main__":
-    seqdir = "/home/zzp/DATABASE/Human/sequences/test/"
+    seqdir = "/home/zzp/DATABASE/Human/sequences/"
     cal(seqdir)
