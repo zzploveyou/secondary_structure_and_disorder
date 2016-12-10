f72e11fe78c5434a53c95071a1d77c80c531d20a
biolab <biolab@mathbio.nankai.edu.cn>
2016-12-10 15:42:23 +0800

    get ptm info of Human sequences.

---
+++ ptm_get.py
@@ -0,0 +1,49 @@
+from Bio import SeqIO
+
+d2p2_protein_to_uniprot_filename = "D2P2/d2p2_protein_to_uniprot.tsv"
+d2p2_fastafile = "D2P2/human.fasta"
+uni_fastafile = "UP000005640_9606.fasta"
+ptm_filename = "D2P2/ptm_assignment.tsv"
+ptm_result = "D2P2/PTM_human.txt"
+def getidlens(fastafile, tag="normal"):
+    m = {}
+    for rec in SeqIO.parse(fastafile, "fasta"):
+        if tag == "d2p2":
+            m[rec.id] = len(rec.seq)
+        elif tag == "normal":
+            m[rec.id.split("|")[1]] = len(rec.seq)
+    return m
+
+def getmap():
+    """return a dic, map uniprot id to d2p2 id"""
+    human_IDs = ""
+    m = {}
+    for line in open(d2p2_protein_to_uniprot_filename):
+        tmp = line.strip().split()
+        m[tmp[1]] = tmp[0]
+        # print tmp[0], tmp[1]
+    return m
+
+def main():
+    m_d2p2 = getidlens(d2p2_fastafile, tag="d2p2")
+    ma = getmap()
+    m_uni = getidlens(uni_fastafile, tag="normal")
+    id_map = {}
+    
+    for idd in m_uni.keys():
+        if idd in ma:
+            if ma[idd] in m_d2p2:
+                if m_d2p2[ma[idd]] == m_uni[idd]:
+                    id_map[ma[idd]]=idd
+    lines = []
+    for line in open(ptm_filename):
+        tmp = line.strip().split("\t")
+        if tmp[2] in id_map:
+            lines.append("%s\t%s\t%s\n" % (id_map[tmp[2]], tmp[3], tmp[4]))
+    fw = open(ptm_result, 'w')
+    fw.writelines(lines)
+    fw.close()
+
+if __name__ == '__main__':
+    main()
+    
